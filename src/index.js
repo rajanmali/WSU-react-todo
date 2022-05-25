3a7e85c7e2a0ac6643b4186c2c9e3aecdf47b590
Rajan Mali <mysteriouskiller50@gmail.com>
2022-05-26 08:42:38 +1000

    added packages for bootstrap and sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Components
 import App from './App';
+
+// Styles
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
